c36f66be4e9932cfdd02f0fc0f091d6899b4d335
Zhang Xinmiao <zxinmiao@upmemcloud5.cloud.upmem.com>
2025-04-27 16:59:07 +0200

    a

--- BFS/run_performance.sh
+++ BFS/run_performance.sh
@@ -4,7 +4,7 @@ make clean
 echo "======"
 echo "Peak performance"
 # NR_DPUS=2500 NR_TASKLETS=1 make all
-NR_DPUS=64 NR_TASKLETS=1 make all
+NR_DPUS=256 NR_TASKLETS=16 make all
 for data in R18-16.el loc-gowalla.el;do
     result_dir=result_cloud/performance/1D_dense_push/${data}
     # result_dir=result_cloud/performance/sparse_push/${data}
@@ -13,7 +13,7 @@ for data in R18-16.el loc-gowalla.el;do
     mkdir -p ${result_dir}
     for i in {1..5}; do
         echo "Run $i for $data"
-        ./bin/host_code -v 1 -f /mnt/data_nvme_2/zhangxinmiao/graph_data2/${data} > ${result_dir}/run_${i}.log
+        ./bin/host_code -v 1 -f ~/${data} > ${result_dir}/run_${i}.log
         # ./bin/host_code -v 1 -f ~/${data} -s 0 > ${result_dir}/run_${i}.log
     done
     # ./bin/host_code -v 1 -f /mnt/data_nvme_2/zhangxinmiao/graph_data2/${data} -s 0 >> ${result_dir}/${data}.log
